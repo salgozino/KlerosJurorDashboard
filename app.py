4d0966c0c6e848162cdac4196615be76483a1229
Santiago Algozino <salgozino@gmail.com>
2020-06-15 14:17:34 -0300

    Fix bug in the new Layout due to wsgi import

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import dash
+from flask import Flask
 
-app = dash.Dash(__name__, suppress_callback_exceptions=True)
+server = Flask(__name__)
+app = dash.Dash(__name__, 
+                server=server,
+                suppress_callback_exceptions=True)
 app.title='Kleros Jurors Dashboard'
-server = app.server
--- application.py
+++ application.py
@@ -4,7 +4,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 
-from app import app
+from app import app, server
 from layouts import layout_es, layout_en
 import callbacks
 
