c5d1d8597117b9b874e158d5a227e3281226516c
Santiago Algozino <salgozino@gmail.com>
2020-06-24 14:14:26 -0300

    Cambios múltiples para generar un nuevo release con una interfaz mejorada. Dejo de usar dash para usar puramente flask, con plotly en JS.
    Relacionado con #2
    Closes #9

--- bin/web3Node.py
+++ bin/web3Node.py
@@ -4,7 +4,9 @@
 from web3 import Web3
 import json
 import os
+import os, sys
 THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+UPPER_FOLDER = os.path.split(THIS_FOLDER)[0]
 
              
 class web3Node():
@@ -12,8 +14,8 @@ class web3Node():
         infura_node = os.environ['INFURA_NODE']
     except:
         print("NO OS ETHERSCAN_KEY VARIABLE FOUND")
-        infura_node = json.load(open(os.path.join(THIS_FOLDER,'static/lib/infura_node.json'),'r'))['http']
-        # infura_node = json.load(open(os.path.join(THIS_FOLDER,'static/lib/infura_node.json'),'r'))['wss']
+        infura_node = json.load(open(os.path.join(UPPER_FOLDER,'lib/infura_node.json'),'r'))['http']
+        # infura_node = json.load(open(os.path.join(UPPER_FOLDER,'lib/infura_node.json'),'r'))['wss']
     web3 = Web3(Web3.HTTPProvider(infura_node))
     # web3 = Web3(Web3.WebsocketProvider(infura_node))
     
