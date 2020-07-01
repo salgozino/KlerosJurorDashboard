0909dfb2c904135b13e63e620c97ed740cbdf18b
Santiago Algozino <salgozino@gmail.com>
2020-07-01 20:04:22 -0300

    Change in getCourtChilds to a json file, so no need to do web3 requests

--- bin/Kleros.py
+++ bin/Kleros.py
@@ -73,7 +73,10 @@ class KlerosLiquid(Contract, web3Node, Etherscan):
 
 
     def getCourtChildrens(self, courtID):
-        return self.contract.functions.getSubcourt(int(courtID)).call()[0]
+        #return self.contract.functions.getSubcourt(int(courtID)).call()[0]
+        with open(os.path.join(UPPER_FOLDER,'data\courtsInformation.json'), 'r') as f:
+            courtsInfo = json.loads(f.read())
+        return courtsInfo[str(courtID)]['childs']
 
 
     def getAllCourtChilds(self, courtID):
