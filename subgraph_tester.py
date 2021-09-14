7bb9062e5ead32f5985e10b57fdcf2788c1fb3af
Blake Haydon <blake.a.haydon@gmail.com>
2021-09-14 21:23:12 +1000

    Delete subgraph_tester

--- subgraph_tester.py
+++
@@ -1,9 +0,0 @@
-from app.modules.subgraph import Subgraph
-
-network = 'test'
-
-subgraph = Subgraph(network)
-
-profile = '0x3590ac9cf1ec55d1fb86993718efabf9f1b9373a'
-netreward = subgraph.getNetRewardProfile(profile)
-print(netreward)
